caf9d738e82c34410b8599b88a6a5c045bba9bd8
Emil Bergström <emil.bergstrom1@gmail.com>
2017-05-17 10:20:08 +0200

    first try on nginx module

---
+++ rate-limit-license/module/ngx_http_license_module.c
@@ -0,0 +1,121 @@
+#include <nginx.h>
+#include <ngx_config.h>
+#include <ngx_core.h>
+#include <ngx_http.h>
+
+#include <string.h>
+
+ngx_module_t ngx_http_license_module;
+
+typedef struct {
+  ngx_uint_t   rate;
+} ngx_http_license_loc_conf_t;
+
+static ngx_int_t ngx_http_license_handler(ngx_http_request_t *r);
+static ngx_int_t ngx_http_license_init(ngx_conf_t *cf);
+static void *    ngx_http_license_create_loc_conf(ngx_conf_t *cf);
+static char *    ngx_http_license_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child);
+
+static ngx_command_t ngx_http_license_module_commands[] = {
+  {
+    ngx_string("license"),
+    NGX_HTTP_MAIN_CONF|NGX_CONF_TAKE1,
+    ngx_conf_set_num_slot,
+    NGX_HTTP_LOC_CONF_OFFSET,
+    offsetof(ngx_http_license_loc_conf_t, rate),
+    NULL
+  }
+};
+
+static ngx_http_module_t ngx_http_license_module_ctx = {
+  NULL,                                 /* preconfiguration */
+  ngx_http_license_init,                /* postconfiguration */
+  NULL,                                 /* create main configuration */
+  NULL,                                 /* init main configuration */
+  NULL,                                 /* create server configuration */
+  NULL,                                 /* merge server configuration */
+  ngx_http_license_create_loc_conf,     /* create location configuration */
+  ngx_http_license_merge_loc_conf       /* merge location configuration */
+};
+
+ngx_module_t ngx_http_license_module = {
+  NGX_MODULE_V1,
+  &ngx_http_license_module_ctx, /* module context */
+  ngx_http_license_module_commands,                            /* module directives */
+  NGX_HTTP_MODULE,                 /* module type */
+  NULL,                            /* init master */
+  NULL,                            /* init module */
+  NULL,                            /* init process */
+  NULL,                            /* init thread */
+  NULL,                            /* exit thread */
+  NULL,                            /* exit process */
+  NULL,                            /* exit master */
+  NGX_MODULE_V1_PADDING
+};
+
+static ngx_int_t ngx_http_license_handler(ngx_http_request_t *r)
+{
+  if (r->main->internal) {
+    return NGX_DECLINED;
+  }
+
+  r->main->internal = 1;
+
+  ngx_http_license_loc_conf_t  *conf;
+  conf = ngx_http_get_module_loc_conf(r, ngx_http_license_module);
+
+  ngx_table_elt_t *h;
+  h = ngx_list_push(&r->headers_out.headers);
+  h->hash = 1;
+
+  char* str = ngx_palloc(r->pool, 20 * sizeof(char));
+  sprintf(str, "Hej %lu", conf->rate);
+
+  ngx_str_set(&h->key, "X-NGINX-Tutorial");
+  ngx_str_set(&h->value, str);
+
+  return NGX_DECLINED;
+}
+
+static ngx_int_t ngx_http_license_init(ngx_conf_t *cf)
+{
+  ngx_http_handler_pt *h;
+  ngx_http_core_main_conf_t *cmcf;
+
+  cmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
+
+  h = ngx_array_push(&cmcf->phases[NGX_HTTP_ACCESS_PHASE].handlers);
+  if (h == NULL) {
+    return NGX_ERROR;
+  }
+
+  *h = ngx_http_license_handler;
+
+  return NGX_OK;
+}
+
+// Configuration
+static void * ngx_http_license_create_loc_conf(ngx_conf_t *cf)
+{
+  ngx_http_license_loc_conf_t  *conf;
+  conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_license_loc_conf_t));
+
+  if (conf == NULL) {
+    return NGX_CONF_ERROR;
+  }
+
+  conf->rate = NGX_CONF_UNSET_UINT;
+
+  return conf;
+}
+
+static char * ngx_http_license_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
+{
+  ngx_http_license_loc_conf_t  *prev = (ngx_http_license_loc_conf_t *)parent;
+  ngx_http_license_loc_conf_t  *conf = (ngx_http_license_loc_conf_t *)child;
+
+  ngx_conf_merge_uint_value(conf->rate, prev->rate, 0);
+
+  return NGX_CONF_OK;
+}
+// End configuration
